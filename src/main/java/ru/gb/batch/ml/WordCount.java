d1c2c83eb51bdd05dad4001291a0dab071ddd19b
cpt2solo <cpt2solo@gmail.com>
2021-06-17 09:24:41 +0200

    Mistake in the push
    Revert "use RainForestClassifier"
    
    This reverts commit 95e4bf3cc714d62d53c1c8490055ea1aa31b985a.

--- src/main/java/ru/gb/batch/ml/WordCount.java
+++ src/main/java/ru/gb/batch/ml/WordCount.java
@@ -15,8 +15,7 @@ import static org.apache.spark.sql.functions.*;
 import org.apache.spark.ml.Pipeline;
 import org.apache.spark.ml.PipelineModel;
 import org.apache.spark.ml.PipelineStage;
-import org.apache.spark.ml.classification.RandomForestClassifier;
-import org.apache.spark.ml.classification.RandomForestClassificationModel;
+import org.apache.spark.ml.classification.LogisticRegression;
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator;
 import org.apache.spark.ml.feature.HashingTF;
 import org.apache.spark.ml.feature.Tokenizer;
@@ -64,9 +63,9 @@ public class WordCount {
   
         HashingTF hashingTF = new HashingTF().setNumFeatures(1000).setInputCol(tokenizer.getOutputCol()).setOutputCol("features");
   
-        RandomForestClassifier rf = new RandomForestClassifier().setFeaturesCol("features").setLabelCol("label");
+        LogisticRegression lr = new LogisticRegression().setFeaturesCol("features").setLabelCol("label").setMaxIter(10).setRegParam(0.001);
   
-        Pipeline pipeline = new Pipeline().setStages(new PipelineStage[]{tokenizer, hashingTF, rf});
+        Pipeline pipeline = new Pipeline().setStages(new PipelineStage[]{tokenizer, hashingTF, lr});
 
         // запускаем обучение
         PipelineModel model = pipeline.fit(dtrain);
@@ -77,9 +76,6 @@ public class WordCount {
         
         // запускаем предсказания
         Dataset<Row> dp = model.transform(dtest);
-
-        // показываем сэмпл предсказаний
-        dp.select("*").show(10);
 
         // проверяем точность модели
         List<Row>  rawResults = dp.withColumn("right", col("prediction").equalTo(col("label"))).groupBy("right").count().collectAsList();
@@ -93,7 +89,7 @@ public class WordCount {
         final double eaccuracy = evaluator.evaluate(dp);
 
         // показываем результаты
-       System.out.println(" RandomForestClassification ");
+       System.out.println(" Logistic Regression ");
        System.out.printf(" Right prediction: %d%n Wrong prediction: %d%n Model accuracy: %.4f%n Accuracy evaluation: %.4f%n",results.get(true), results.get(false), accuracy, eaccuracy);
 
         // завершаем работу
